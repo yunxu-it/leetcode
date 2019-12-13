12dd8d4cf305da096716efc0ef3e5ee9f9e4ce24
lxlong <yunxu.it@outlook.com>
2019-12-13 18:18:03 +0800

    插入排序法

--- java/src/main/sort/Insert.kt
+++ java/src/main/sort/Insert.kt
@@ -3,16 +3,20 @@ package main.sort
 class Insert {
     /**
      * 插入排序法
-     * 在要排序的一组数中，选出最小（或最大）的一个数与第一个位置的数交换；在剩下的数当中找最小的与第二个位置的数交换，即顺序放在已排好序的数列的最后，如此循环，直到全部数据元素排完为止。
+     * 把待排序的记录按其关键码值的大小逐个插入到一个已经排好序的有序序列中，直到所有的记录插入完为止，得到一个新的有序序列
      */
     fun sort(nums: IntArray) {
-        for (i in 1..nums.size - 1) {
-            for (j in i - 1 downTo 0) {
-                if (nums[j] < nums[j + 1]) {
-                    Utils.exchange(nums, j, i)
+        for (i in 1 until nums.size) {
+            if (nums[i - 1] > nums[i]) {
+                val temp = nums[i]
+                var j = i
+                while (j > 0 && nums[j - 1] > temp) {
+                    nums[j] = nums[j - 1]
+                    j--
+//                    println(nums.toList().toString())
                 }
+                nums[j] = temp;
             }
-            println(nums.toList().toString())
         }
     }
 }
