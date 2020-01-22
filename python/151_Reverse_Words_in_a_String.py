340534e9d19d6d334baa3bc4fb40cb8eb13eeb90
lxlong <yunxu.it@outlook.com>
2020-01-22 21:13:09 +0800

    java 分支项目配置

--- python/151_Reverse_Words_in_a_String.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# encoding: utf-8
-
-class Solution:
-    # @param s, a string
-    # @return a string
-    def reverseWords(self, s):
-        return ' '.join(reversed(s.split()))
